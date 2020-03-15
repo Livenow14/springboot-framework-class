37ac9c1b633189f8e9127257d5777bb7376ec386
Livenow14 <xowjd41@naver.com>
2020-03-15 14:03:31 +0900

    Environment 2 - Property

--- src/main/java/me/livenow/spirngframework/demospringboot/DemospringbootApplication.java
+++ src/main/java/me/livenow/spirngframework/demospringboot/DemospringbootApplication.java
@@ -2,8 +2,10 @@ package me.livenow.spirngframework.demospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/app.properties")
 public class DemospringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemospringbootApplication.class, args);
